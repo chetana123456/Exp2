c174a520c718771ed12e46e9ab6346d681d47634
chetana <chetanat26@gmail.com>
2024-07-19 15:22:25 +0530

    modified the file add.java

---
+++ Add.java
@@ -0,0 +1,7 @@
+class Add
+{
+    public static void main(String s[])
+    {
+        System.out.println("hii")
+    }
+}
---
+++ Sub.java
@@ -0,0 +1,7 @@
+class Sub
+{
+    public static void main(String s[])
+    {
+        System.out.println("hii")
+    }
+}
